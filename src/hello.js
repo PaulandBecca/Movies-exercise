23f9c8a248e09640c127b7c6764dd4f9b93ad7a1
paulvaldez <valdezpaul00@gmail.com>
2017-09-13 17:06:01 -0500

    Added a loading screen and css

--- src/hello.js
+++ src/hello.js
@@ -1,10 +1,24 @@
-window.loading_screen = window.pleasewait({
-    logo: "assets/images/logo.png",
-    backgroundColor: '#f46d3b',
-    loadingHtml:"loading.."
+
+
+function onReady(callback) {
+    let intervalID = window.setInterval(checkReady, 1000);
+    function checkReady() {
+        if (document.getElementsByTagName('body')[0] !== undefined) {
+            window.clearInterval(intervalID);
+            callback.call(this);
+        }
+    }
+}
+
+
+function show(id, value) {
+    document.getElementById(id).style.display = value ? 'block' : 'none';
+}
+
+onReady(function () {
+    show('page', true);
+    show('loading', false);
 });
-
-
 
 
 
