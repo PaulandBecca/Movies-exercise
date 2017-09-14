0ed8cdc1806d5964cada46cb9d6f40554bc96216
becca <r.alexis.tolentino@gmail.com>
2017-09-14 11:23:30 -0500

    form input and buttons add functions to index js

--- src/index.js
+++ src/index.js
@@ -34,11 +34,47 @@ onReady(function () {
 const getMovies = require('./getMovies.js');
 
 getMovies().then((movies) => {
-  console.log('Here are all the movies:');
+ document.getElementById("loading").innerHTML = 'Here are the movies:'
   movies.forEach(({title, rating, id}) => {
-    console.log(`id#${id} - ${title} - rating: ${rating}`);
+      addMovieToHtml(title, rating)
+
   });
 }).catch((error) => {
   alert('Oh no! Something went wrong.\nCheck the console for details.');
   console.log(error);
+
 });
+
+//here we have our stringify object
+let movieTitle = document.getElementById("Title").value;
+let movieRating = document.getElementById("rating").value;
+let header = new Headers({
+    'content-Type': 'application/json'
+});
+
+let movieObj = {title:movieTitle, rating: movieRating};
+console.log(movieObj);
+let fetchOptions ={
+    method: "POST",
+    body: JSON.stringify(movieObj),
+    headers: header
+};
+document.getElementById("title").value = "";
+document.getElementById("rating").value = "1";
+fetch("/api/movies", fetchOptions)
+    .then((response)=>console.log(response.json()));
+addMovieToHtml(movieTitle,movieRating)
+
+}
+//event listener for button
+document.getElementById("button").addEventListener("click, addAMovie");
+
+//add to html
+function addMovieToHtml ( title, rating){
+    document.getElementById("movie-list").innerHTML += `<li>${title} - rating: ${rating}</li>`
+}
+
+//module.exports = getMovies;
+
+
+
